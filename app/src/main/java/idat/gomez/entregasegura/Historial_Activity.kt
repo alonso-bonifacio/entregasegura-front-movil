cadec572f871809ee4fd63c1cecb9670a209fc2c
Selene Gomez <alisonpantoja635@gmail.com>
2023-11-22 19:11:22 -0500

    ventana de datos de envio

---
+++ app/src/main/java/idat/gomez/entregasegura/Historial_Activity.kt
@@ -0,0 +1,11 @@
+package idat.gomez.entregasegura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Historial_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historial)
+    }
+}
