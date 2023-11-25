fe8a46bf22f543b25a5fbc3a7ae0967585c5c797
Selene Gomez <alisonpantoja635@gmail.com>
2023-11-25 12:10:56 -0500

    ventana de Historial lista, y se inplementaron iconos en otras ventanas

---
+++ app/src/main/java/idat/gomez/entregasegura/DatosEnvio.java
@@ -0,0 +1,14 @@
+package idat.gomez.entregasegura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosEnvio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_envio);
+    }
+}
