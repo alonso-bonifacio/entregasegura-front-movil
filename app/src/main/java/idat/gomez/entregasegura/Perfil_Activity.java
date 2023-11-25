336ee689c8f95613cbdbbfa6a922869b50dcf42e
Selene Gomez <alisonpantoja635@gmail.com>
2023-11-25 16:06:47 -0500

    vista de perfil y modificaciones en otras ventanas

---
+++ app/src/main/java/idat/gomez/entregasegura/Perfil_Activity.java
@@ -0,0 +1,14 @@
+package idat.gomez.entregasegura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
