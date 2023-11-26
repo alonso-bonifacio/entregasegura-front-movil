a4cc98cfd74f8abb44d03fd4919ad0cd5cd5bf12
Yomar <alonso.aybp@gmail.com>
2023-11-26 12:59:15 -0500

    Se corrigio errores de compilacion

---
+++ app/src/main/java/idat/gomez/entregasegura/Historial_Activity.java
@@ -0,0 +1,12 @@
+package idat.gomez.entregasegura;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Historial_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+    }
+}
